c835df0fed4bf02708a7188a79cf2ab5eb3eef43
Don Goodman-Wilson <don@slack-corp.com>
2018-01-11 17:38:14 +0100

    This _should_ work. Maybe. Mostly. Fingers crossed.

--- conanfile.py
+++ conanfile.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from conans import ConanFile, CMake, tools
+from conans import ConanFile, AutoToolsBuildEnvironment, tools
 import os
 
 
@@ -18,6 +18,7 @@ class LibmagicConan(ConanFile):
     #use static org/channel for libs in conan-center
     #use version ranges for dependencies unless there's a reason not to
     requires = "zlib/[>=1.2.8]@conan/stable"
+    source_subfolder = "sources"
         
     # def requirements(self):
     #     #use dynamic org/channel for libs in bincrafters
@@ -28,24 +29,85 @@ class LibmagicConan(ConanFile):
         filename_root = "FILE{0}".format(self.version.replace('.', '_'))
         tools.get("{0}/archive/{1}.tar.gz".format(source_url, filename_root))
         extracted_dir = "file-{0}".format(filename_root)
-        os.rename(extracted_dir, "sources")
+        os.rename(extracted_dir, self.source_subfolder)
         #Rename to "sources" is a convention to simplify later steps
 
+    def configure(self):
+        del self.settings.compiler.libcxx
+
+
+    def _build_mingw(self):
+        env_build = AutoToolsBuildEnvironment(self)
+        env_build.fpic = True
+        unix_environment = {}
+        for key, value in env_build.vars.items():
+            unix_environment[key] = value.replace("\\", "/")
+        with tools.environment_append(unix_environment):
+            configure_args = ['--prefix="%s"' % self.package_folder]
+            configure_args.append('--enable-shared' if self.options.shared else '--disable-shared')
+            configure_args.append('--enable-static' if not self.options.shared else '--disable-static')
+            if self.settings.arch == "x86_64":
+                configure_args.append('--host=x86_64-w64-mingw32')
+            if self.settings.arch == "x86":
+                configure_args.append('--build=i686-w64-mingw32')
+                configure_args.append('--host=i686-w64-mingw32')
+            with tools.chdir(self.source_subfolder):
+                tools.run_in_windows_bash(self, tools.unix_path("autoreconf -f -i"))
+                tools.run_in_windows_bash(self, tools.unix_path("./configure %s" % ' '.join(configure_args)))
+                tools.run_in_windows_bash(self, tools.unix_path("make"))
+                tools.run_in_windows_bash(self, tools.unix_path("make install"))
+
+    def _build_macos(self):
+        env_build = AutoToolsBuildEnvironment(self)
+        env_build.fpic = True
+        with tools.environment_append(env_build.vars):
+            configure_args = ['--prefix=%s' % self.package_folder]
+            configure_args.append('--enable-shared' if self.options.shared else '--disable-shared')
+            configure_args.append('--enable-static' if not self.options.shared else '--disable-static')
+            with tools.chdir(self.source_subfolder):
+                self.run("autoreconf -f -i")
+                env_build.configure(args=configure_args)
+                env_build.make(args=["all"])
+                env_build.make(args=["install"])
+
+    def _build_linux(self):
+        env_build = AutoToolsBuildEnvironment(self)
+        env_build.fpic = True
+        with tools.environment_append(env_build.vars):
+            configure_args = ['--prefix=%s' % self.package_folder]
+            configure_args.append('--enable-shared' if self.options.shared else '--disable-shared')
+            configure_args.append('--enable-static' if not self.options.shared else '--disable-static')
+            with tools.chdir(self.source_subfolder):
+                env_build.run("autoreconf -f -i")
+                env_build.configure(args=configure_args)
+                env_build.make(args=["all"])
+                env_build.make(args=["install"])
+
     def build(self):
-        cmake = CMake(self)
-        cmake.definitions["BUILD_TESTS"] = False # example
-        cmake.configure(source_dir="sources")
-        cmake.build()
+        if self.settings.os == "Windows" and self.settings.compiler == "gcc":
+            self._build_mingw()
+        elif self.settings.os == "Windows" and self.settings.compiler == "Visual Studio":
+            #self._build_visual_studio()
+            self.output.error("Cannot build with Visual Studio")
+            exit(-1)
+        elif self.settings.os == "Linux":
+            self._build_linux()
+        else:
+            self._build_macos()
+        
 
     def package(self):
         with tools.chdir("sources"):
-            self.copy(pattern="LICENSE")
-            self.copy(pattern="*", dst="include", src="include")
-            self.copy(pattern="*.dll", dst="bin", src="bin", keep_path=False)
-            self.copy(pattern="*.lib", dst="lib", src="lib", keep_path=False)
-            self.copy(pattern="*.a", dst="lib", src="lib", keep_path=False)
-            self.copy(pattern="*.so*", dst="lib", src="lib", keep_path=False)
-            self.copy(pattern="*.dylib", dst="lib", src="lib", keep_path=False)
+            self.copy(pattern="COPYING")
+            # self.copy(pattern="magic.h", dst="include", src="src")
+            # self.copy(pattern="*.dll", dst="bin", src="bin", keep_path=False)
+            # self.copy(pattern="*.lib", dst="lib", src="lib", keep_path=False)
+            # self.copy(pattern="*.a", dst="lib", src="lib", keep_path=False)
+            # self.copy(pattern="*.so*", dst="lib", src="lib", keep_path=False)
+            # self.copy(pattern="*.dylib", dst="lib", src="lib", keep_path=False)
+            # self.copy("magic.mgc", dst="share/misc", src="magic", keep_path=False)
+            # self.copy(pattern="*", dst="share/misc/magic", src="magic/Magdir", keep_path=False)
+
 
     def package_info(self):
         self.cpp_info.libs = tools.collect_libs(self)
